0b2af8ef3b0b7d6599bf6ce7bee2866c2ab1da36
Araryarch <araryaarch@gmail.com>
2024-10-27 00:19:01 +0700

    feat: doing nothing

--- app/(logged-out)/Layout.tsx
+++ app/(logged-out)/Layout.tsx
@@ -4,8 +4,6 @@ import { PersonStandingIcon } from 'lucide-react'
 import ThemeToggle from '../../components/ui/theme-toggle'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import darkBg from '@/app/bg-dark.png'
-import lightBg from '@/app/bg-light.png'
 
 type Props = {
   children: React.ReactNode
@@ -16,25 +14,25 @@ const Layout = ({ children }: Props) => {
   const isRoot = pathname === '/'
 
   return (
-    <div
-      className={`flex min-h-screen w-full flex-col items-center justify-center bg-[url(${lightBg})] bg-cover py-10 transition-all duration-500 ease-in-out dark:bg-[url(${darkBg})]`}
-    >
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-[url(bg-light.png)] bg-cover py-10 transition-all duration-500 ease-in-out dark:bg-[url(bg-dark.png)]">
       <div className="py-2">
         <PersonStandingIcon size={45} />
       </div>
       <div
-        className={`flex flex-col items-center justify-center rounded-sm ${isRoot ? '' : 'border-[1px]'} border-secondary px-2 py-4 backdrop-blur-lg dark:border-white`}
+        className={`flex flex-col items-center justify-center rounded-sm ${isRoot ? '' : 'border-[1px] backdrop-blur-lg'} border-secondary px-2 py-4 dark:border-white`}
       >
         {children}
       </div>
       <div className="absolute right-0 top-0 p-10">
         <ThemeToggle />
       </div>
-      <div className="absolute left-0 top-0 p-10">
-        <Link href={'/'}>
-          <p>BACK TO HOME</p>
-        </Link>
-      </div>
+      {!isRoot && (
+        <div className="absolute left-0 top-0 p-10">
+          <Link href={'/'}>
+            <p>BACK TO HOME</p>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
